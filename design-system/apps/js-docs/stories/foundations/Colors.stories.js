93504a435fcbf6de67ff1f52380d4174ea65bbd8
Ana Takahashi <contato@anatakahashi.com.br>
2026-02-10 19:16:27 -0300

    new action rules

---
+++ design-system/apps/js-docs/stories/foundations/Colors.stories.js
@@ -0,0 +1,19 @@
+import { ColorsTable } from './Colors.mdx';
+
+export default {
+  title: 'Foundations/Colors',
+  component: ColorsTable,
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'Tokens de cor do pacote @jellyfish/tokens, filtrados por $type: "color".',
+      },
+    },
+  },
+};
+
+export const AllColors = {
+  name: 'Colors',
+};
+
